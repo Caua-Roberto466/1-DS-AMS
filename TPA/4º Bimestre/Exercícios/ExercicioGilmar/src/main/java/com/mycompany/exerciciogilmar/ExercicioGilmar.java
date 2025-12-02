6a229b0fee8b6ce583542f91f2ba3a72b4eec5dc
Caua-Roberto466 <cauaroberto211@gmail.com>
2025-12-02 20:48:11 -0300

    Exercício do GIlmar corrigido

--- TPA/4º Bimestre/Exercícios/ExercicioGilmar/src/main/java/com/mycompany/exerciciogilmar/ExercicioGilmar.java
+++ TPA/4º Bimestre/Exercícios/ExercicioGilmar/src/main/java/com/mycompany/exerciciogilmar/ExercicioGilmar.java
@@ -8,11 +8,24 @@ public class ExercicioGilmar {
         
         String[][] Matriz = new String[3][4];
         
-        for(int i = 0; i < Matriz.length; i++){
-            for(int j = 0; j < Matriz[i].length; j++){
-                System.out.print("Digite o valor ");
-                Matriz[i][j] = scan.nextLine();
-            }
+        Matriz[0][0] = "Nome";
+        Matriz[0][1] = "nota1";
+        Matriz[0][2] = "nota2";
+        Matriz[0][3] = "media";
+        
+        for(int i = 1; i < Matriz.length; i++){
+            System.out.println("Aluno "+i);
+            System.out.print("Digite o Nome: ");
+            Matriz[i][0] = scan.nextLine();
+                
+            System.out.print("Digite a nota 1: ");
+            Matriz[i][1] = scan.nextLine();
+            
+            System.out.print("Digite a nota 2: ");
+            Matriz[i][2] = scan.nextLine();
+            
+            double media = (Double.valueOf(Matriz[i][1]) + Double.valueOf(Matriz[i][2]))/2;
+            Matriz[i][3] = String.valueOf(media);
         }
         
         for(int i = 0; i < Matriz.length; i++){
@@ -21,5 +34,6 @@ public class ExercicioGilmar {
             }
             System.out.println("");
         }
+        
     }
 }
